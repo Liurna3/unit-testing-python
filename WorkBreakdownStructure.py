d343aa6bf2170426fe08a6dbf45b35b16f4b9323
Luis Galindo <egalindo54@uabc.edu.mx>
2022-11-06 19:56:59 -0800

    clase WorkBreakdownStructure agregada

---
+++ WorkBreakdownStructure.py
@@ -0,0 +1,17 @@
+class WorkBreakdownStructure():
+    def __init__(self):
+        self.name = ""
+        self.theTasks = ""
+
+    def getName(self):
+        return self.name
+
+    def getTheTask(self):
+        return self.theTasks
+
+    def setName(self, name):
+        self.name = name
+
+    def setTheTask(self, theTasks):
+        self.theTasks = theTasks
+
